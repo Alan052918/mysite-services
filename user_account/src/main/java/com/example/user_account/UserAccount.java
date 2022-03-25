10a9b929f2c454b8ee6d82d02b4ffb079ec38878
Alan052918 <aijunda29@gmail.com>
2022-03-25 03:17:42 -0400

    Remove redundant methods in UserAccount

--- user_account/src/main/java/com/example/user_account/UserAccount.java
+++ user_account/src/main/java/com/example/user_account/UserAccount.java
@@ -1,14 +1,17 @@
 package com.example.user_account;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -24,79 +27,17 @@ public class UserAccount {
     private LocalDateTime dateTimeCreated;
     private LocalDateTime dateTimeUpdated;
 
-    public UserAccount(String name, String email, LocalDateTime dateTimeCreated, LocalDateTime dateTimeUpdated) {
-        this.name = name;
-        this.email = email;
-        this.dateTimeCreated = dateTimeCreated;
-        this.dateTimeUpdated = dateTimeUpdated;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDateTime getDateTimeCreated() {
-        return dateTimeCreated;
-    }
-
-    public void setDateTimeCreated(LocalDateTime dateTimeCreated) {
-        this.dateTimeCreated = dateTimeCreated;
-    }
-
-    public LocalDateTime getDateTimeUpdated() {
-        return dateTimeUpdated;
-    }
-
-    public void setDateTimeUpdated(LocalDateTime dateTimeUpdated) {
-        this.dateTimeUpdated = dateTimeUpdated;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
         UserAccount that = (UserAccount) o;
-        return id.equals(that.id) &&
-                Objects.equals(name, that.name) &&
-                Objects.equals(email, that.email) &&
-                Objects.equals(dateTimeCreated, that.dateTimeCreated) &&
-                Objects.equals(dateTimeUpdated, that.dateTimeUpdated);
+        return id != null && Objects.equals(id, that.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, email, dateTimeCreated, dateTimeUpdated);
-    }
-
-    @Override
-    public String toString() {
-        return "UserAccount{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", dateTimeCreated=" + dateTimeCreated +
-                ", dateTimeUpdated=" + dateTimeUpdated +
-                '}';
+        return getClass().hashCode();
     }
 
 }
